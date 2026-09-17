c443408c1929772d082c02f8bf32c5b6254df8db
zeus0390 <zeusjamesbaltazar@gmail.com>
2026-09-17 14:23:49 +0800

    test: code clean up and remove redundant header

--- tests/client.cpp
+++ tests/client.cpp
@@ -4,11 +4,15 @@
 #include <fstream>
 #include <thread>
 #include <chrono>
+#include <atomic>
 #include <ios>
 
 #include "core/network/ClientMessageHandler.hpp"
 #include "core/types/MessageType.hpp"
 #include "core/network/Client.hpp"
+
+std::atomic<bool> is_running {true};
+std::atomic<int> socket_descriptor {-1};
 
 // Main entry point of the program
 int main(int argc, char* argv[])
@@ -25,48 +29,51 @@ int main(int argc, char* argv[])
   ClientMessageHandler client_message_handler;
   Client client {client_message_handler};
 
-  int server_socket_descriptor = client.connect(argv[1], atoi(argv[2]));
+  socket_descriptor.store(client.connect(argv[1], atoi(argv[2])));
 
-  if (server_socket_descriptor < 0)
+  if (socket_descriptor < 0)
   {
     std::cout << "Error connecting to the server." << std::endl;
     return 1;
   }
 
+  std::cout << "Press enter to continue..." << std::endl;
+  std::cin.get();
+
   client.start();
 
-  std::cout << "Press enter to continue..." << std::endl;
+  std::thread sendThread {[&client, argv](){
+    std::fstream file{argv[4], std::ios::in};
+    if (!(file.is_open()))
+    {
+      std::cerr << "Error opening the file." << std::endl;
+      return;
+    }
+    char ch;
+    while (file.get(ch) && is_running.load())
+    {
+      ssize_t send_status;
+      send_status = client.send(
+        socket_descriptor,
+        MessageType::MESSAGE,
+        std::string(1, ch).c_str(),
+        1
+      );
+      if (send_status < 0)
+        std::cout << "Error sending the message to server." << std::endl;
+      std::this_thread::sleep_for(std::chrono::milliseconds(std::stoi(argv[3])));
+    }
+  }};
+
   std::cin.get();
 
-  std::fstream file{argv[4], std::ios::in};
+  is_running.store(false);
 
-  if (!(file.is_open()))
-  {
-    std::cerr << "Error opening the file." << std::endl;
-    return 1;
-  }
-
-  char ch;
-  while (file.get(ch))
-  {
-    ssize_t send_status;
-    send_status = client.send(
-      server_socket_descriptor,
-      MessageType::MESSAGE,
-      std::string(1, ch).c_str(),
-      1
-    );
-    if (send_status < 0)
-      std::cout << "Error sending the message to server." << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(std::stoi(argv[3])));
-  }
-
-  std::cout << "Press enter to continue..." << std::endl;
-  std::cin.get();
-
-  client.disconnect(server_socket_descriptor);
+  client.disconnect(socket_descriptor);
 
   client.stop();
+
+  sendThread.join();
 
   return 0;
 }
--- tests/peer.cpp
+++ tests/peer.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <atomic>
 
 #include "core/network/ClientMessageHandler.hpp"
 #include "core/network/ServerMessageHandler.hpp"
 #include "core/types/MessageType.hpp"
 #include "core/network/Peer.hpp"
+
+std::atomic<bool> is_running {true};
+std::atomic<int> socket_descriptor {-1};
 
 // Main entry point of the program
 int main(int argc, char* argv[])
@@ -23,62 +27,48 @@ int main(int argc, char* argv[])
 
   peer.start(std::stoi(argv[1]));
 
-  int socket_descriptor {peer.connect("localhost", std::stoi(argv[1]))};
+  socket_descriptor.store(peer.connect("localhost", std::stoi(argv[1])));
 
-  std::cin.get();
-
-  std::fstream file{argv[3], std::ios::in};
-
-  if (!(file.is_open()))
+  if (socket_descriptor < 0)
   {
-    std::cerr << "Error opening the file." << std::endl;
+    std::cout << "Error connecting to the server." << std::endl;
     return 1;
   }
 
-  char ch;
-  while (file.get(ch))
-  {
-    MessageHeader message_header;
-    message_header.type = MessageType::MESSAGE;
-    message_header.payload_size = 1;
+  std::cin.get();
 
-    ssize_t send_status;
-
-    send_status = peer.send(
-      socket_descriptor,
-      MessageType::MESSAGE,
-      reinterpret_cast<char*>(&message_header),
-      sizeof(message_header)
-    );
-
-    if (send_status < 0)
+  std::thread sendThread {[&peer, argv](){
+    std::fstream file{argv[3], std::ios::in};
+    if (!(file.is_open()))
     {
-      std::cout << "Error sending the message to server." << std::endl;
-      break;
+      std::cerr << "Error opening the file." << std::endl;
+      return;
     }
-
-    send_status = peer.send(
+    char ch;
+    while (file.get(ch) && is_running.load())
+    {
+      ssize_t send_status;
+      send_status = peer.send(
         socket_descriptor,
         MessageType::MESSAGE,
         std::string(1, ch).c_str(),
-        message_header.payload_size
-    );
-
-    if (send_status < 0)
-    {
-      std::cout << "Error sending the message to server." << std::endl;
-      break;
+        1
+      );
+      if (send_status < 0)
+        std::cout << "Error sending the message to server." << std::endl;
+      std::this_thread::sleep_for(std::chrono::milliseconds(std::stoi(argv[2])));
     }
+  }};
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(std::stoi(argv[2])));
-  }
-
-  std::cout << "Press enter to continue..." << std::endl;
   std::cin.get();
 
   peer.disconnect(socket_descriptor);
 
   peer.stop();
+
+  is_running.store(false);
+
+  sendThread.join();
 
   return 0;
 }
